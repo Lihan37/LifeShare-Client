a6c5c28b59b18cad21c7dba21edb0a4c9f7fcbd6
Eanur Lihan <lihan.lihu@gmail.com>
2023-11-26 16:11:43 +0600

    I have updated Banner section and added it to home

--- src/pages/Shared/navbar/NavBar.jsx
+++ src/pages/Shared/navbar/NavBar.jsx
@@ -65,7 +65,7 @@ const NavBar = () => {
             </Helmet> */}
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost text-gray-300 lg:hidden">
+                    <label tabIndex={0} className="btn btn-ghost text-gray-950 lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 lg:text-gray-950 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
